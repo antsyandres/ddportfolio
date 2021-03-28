3869a8eee29a24164d1534264054ff3c07a5bcf8
antsy <antsyandres@gmail.com>
2021-03-29 01:52:51 +0800

    disable autologin after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+
 class RegisterController extends Controller
 {
     /*
@@ -23,6 +26,21 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+
+/**
+ * Handle a registration request for the application.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\Response
+ */
+public function register(Request $request)
+{
+    $this->validator($request->all())->validate();
+    event(new Registered($user = $this->create($request->all())));
+    // $this->guard()->login($user);
+    return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+ }
 
     /**
      * Where to redirect users after registration.
